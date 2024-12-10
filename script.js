8e93dd3cf2b37f5f92ca506c9cc2d70d0f5d927c
raeoffrost <raeoffrost@gmail.com>
2024-12-09 23:32:58 -0600

    Add getTotalValue() method

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// Create a Base Class: ProductProperties:
+//  ProductProperties:
 //     name (string): Name of the product.
 //     price (number): Price of the product.
 //     quantity (number): Quantity available in stock.
 
     class ProductProperties{
+        //     constructor: Initializes the properties.
         constructor(name, price, quantity) {
             this.name = name;
             this.price = price;
             this.quantity = quantity;
         }
-    }
 
-    //     Methods:
-//     constructor: Initializes the properties.
-//     getTotalValue(): Returns the total value of the product in stock (price * quantity).
-//     toString(): Returns a string representation of the product (e.g., "Product: Apple, Price: $2.50, Quantity: 50").
+        getTotalValue(price, quantity) {
+        //     getTotalValue(): Returns the total value of the product in stock (price * quantity).
+            const total = (price * quantity);
+            console.log(`Total Value: ${total}`);
+        }
+        toString() {
+        //     toString(): Returns a string representation of the product (e.g., "Product: Apple, Price: $2.50, Quantity: 50").
+        }
+    }
